0c7de83cbe2b154e911cb5ae4b80b8e4ce28ad81
platofff <42435266+platofff@users.noreply.github.com>
2020-10-01 20:14:35 +0300

    Create casino.js

---
+++ casino.js
@@ -0,0 +1,17 @@
+let progress = document.getElementsByClassName('progress-bar')[0]
+let percentage = 0
+let percentageText = document.getElementById('percentage')
+const tRoubles = Math.floor(Math.random() * 50000000) + 50000000
+document.getElementById('tRoubles').innerText = tRoubles
+let cRoubles = document.getElementById('cRoubles')
+const yes = () => {
+    percentage += 1
+    if (percentage < 100) {
+        progress.setAttribute('aria-valuenow', percentage)
+        let p = String(percentage) + '%'
+        progress.style.width = p
+        progress.innerText = p
+        percentageText.innerText = p
+        cRoubles.innerText = Math.floor(tRoubles / 100 * percentage)
+    }
+}
