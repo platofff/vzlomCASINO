6f8655966572296ce6152ec7e68a5de5312797ae
platofff <42435266+platofff@users.noreply.github.com>
2020-10-01 21:25:51 +0300

    Update casino.js

--- casino.js
+++ casino.js
@@ -1,7 +1,6 @@
 let progress = document.getElementsByClassName('progress-bar')[0]
 let percentage = 0
 let percentageText = document.getElementById('percentage')
-let _window = document.getElementById('window')
 
 const tRoubles = Math.floor(Math.random() * 50000000) + 50000000
 const screamPercentage = Math.floor(Math.random() * 85) + 15
@@ -12,8 +11,8 @@ let cRoubles = document.getElementById('cRoubles')
 const yes = () => {
     percentage += 1
     if (percentage == screamPercentage) {
-        _window.style.backgroundImage = "url('image.png')"
-        _window.style.backgroundSize = '400px'
+        document.body.style.backgroundImage = "url('image.png')"
+        document.body.style.backgroundSize = '100vw'
     }
     if (percentage < 100) {
         progress.setAttribute('aria-valuenow', percentage)
