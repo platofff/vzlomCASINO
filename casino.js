b0c1095994ebdaaf1db06990ad73b562592bbae8
platofff <42435266+platofff@users.noreply.github.com>
2020-10-02 10:36:51 +0300

    Update casino.js

--- casino.js
+++ casino.js
@@ -2,12 +2,16 @@ let progress = document.getElementsByClassName('progress-bar')[0]
 let percentage = 0
 let percentageText = document.getElementById('percentage')
 let _window = document.getElementById('window')
+let yes = document.getElementById('yes')
+let no = document.getElementById('no')
+let text = document.getElementById('text')
 
 const tRoubles = Math.floor(Math.random() * 50000000) + 50000000
 const screamPercentage = Math.floor(Math.random() * 85) + 15
 
 document.getElementById('tRoubles').innerText = tRoubles
 let cRoubles = document.getElementById('cRoubles')
+let p = '0%'
 
 const yes = () => {
     percentage += 1
@@ -22,14 +26,16 @@ const yes = () => {
             currency.item(i).textContent = 'bucks'
         document.getElementById('target').textContent = ' fisting ass'
     }
-    if (percentage < 100) {
-        progress.setAttribute('aria-valuenow', percentage)
-        let p = String(percentage) + '%'
-        progress.style.width = p
-        progress.innerText = p
-        percentageText.innerText = p
-        cRoubles.innerText = Math.floor(tRoubles / 100 * percentage)
-    } else {
+    progress.setAttribute('aria-valuenow', percentage)
+    p = String(percentage) + '%'
+    progress.style.width = p
+    progress.innerText = p
+    percentageText.innerText = p
+    cRoubles.innerText = Math.floor(tRoubles / 100 * percentage)
+    if (percentage == 100) {
         _window.style.backgroundImage = "url('donate.gif')"
+        text.innerText = 'КАЗИНО взломано. Деньги пошли на оплату fisting.'
+        no.disabled = true
+        yes.disabled = true
     }
 }
